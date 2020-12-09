a1138dd3e16b538cbaf571bf05ebc8792f39c4c3
caobandoc <obandocarlosandres@gmail.com>
2020-12-09 10:43:57 -0500

    Test de propiedades

--- src/main/java/com/clearminds/caoc/bdd/ConexionBDD.java
+++ src/main/java/com/clearminds/caoc/bdd/ConexionBDD.java
@@ -1,5 +1,19 @@
 package com.clearminds.caoc.bdd;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConexionBDD {
 
+	public static String leerPropiedad(String propiedad){
+		Properties p = new Properties();
+		try {
+			p.load(new FileReader("conexion.properties"));
+			return p.getProperty(propiedad);
+		} catch (IOException e) {
+			return null;
+		}
+		 
+	}
 }
