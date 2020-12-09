6db350e1ebb0c1a2502892cc51e27335c4e7c846
caobandoc <obandocarlosandres@gmail.com>
2020-12-09 13:17:01 -0500

    Agregando metodo insertarEstudiante

--- src/main/java/com/clearminds/caoc/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/caoc/servicios/ServicioEstudiante.java
@@ -1,5 +1,9 @@
 package com.clearminds.caoc.servicios;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.clearminds.caoc.bdd.ConexionBDD;
 import com.clearminds.caoc.dtos.Estudiante;
 import com.clearminds.caoc.excepciones.BDDException;
 
@@ -8,7 +12,21 @@ public class ServicioEstudiante extends ServicioBase {
 	public void insertarEstudiante(Estudiante estudiante) throws BDDException {
 		abrirConexion();
 		System.out.println("Insertando estudiante: " + estudiante.toString());
-		cerrarConexion();
+		Statement stmt = null;
+		try {
+			stmt = ConexionBDD.obtenerConexion().createStatement();
+
+			String sql = "insert into estudiantes(nombre,apellido) values('" + estudiante.getNombre() + "','"
+					+ estudiante.getApellido() + "')";
+			
+			System.out.println("Script: "+sql);
+			
+			stmt.executeUpdate(sql);
+			cerrarConexion();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new BDDException("Error al insertar estudiante");
+		}
 
 	}
 }
